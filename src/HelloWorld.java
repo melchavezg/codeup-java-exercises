1409e0e33ab5b46f057a1b57c85fa0133e268c03
melchavezg <mchavezg300@gmail.com>
2023-05-15 10:54:18 -0500

    finished

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,55 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        int myFavoriteNumber = 8;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "This is a string";
+//        System.out.println(myString);
+//        String myString = 'm';
+//        System.out.println(myString);
+//        float myString = 3.14159;
+//        System.out.println(myString);
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        ^ doesn't print immediately
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        ^ prints immediately
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(x);
+//        System.out.println(y);
+//        int x = 3;
+//        int y = 4;
+//        y *= 3;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(x);
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
     }
 }
