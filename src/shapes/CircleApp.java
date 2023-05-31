d9b56b5f669043ee37cc852dd495d6acb8715b51
melchavezg <mchavezg300@gmail.com>
2023-05-31 14:04:50 -0500

    final commit

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,20 @@
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input in = new Input();
+
+        do {
+            System.out.println("Enter a radius to solve for area and circumference");
+            double radius = in.getDouble();
+            Circle cir = new Circle(radius);
+
+            System.out.println("This is the area " + cir.getArea());
+            System.out.println("This is the circumference " + cir.getCircumference());
+
+            in.getString();
+        } while (in.yesNo());
+    }
+}
